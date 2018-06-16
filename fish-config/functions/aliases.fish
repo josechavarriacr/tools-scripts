ca6092f3dfb28ccff975f214ea40a075fbea99bf
Jose Chavarria <jose.chavarria@grupoardigital.com>
2018-06-16 13:22:36 -0600

    import fish configurations inclde alias

---
+++ fish-config/functions/aliases.fish
@@ -0,0 +1,41 @@
+
+################################
+###  Command ShortCut
+################################
+alias ..="cd .."
+# List only directories
+alias lsd='ls -l | grep "^d"'
+#list all information
+alias lsa="ls -la"
+alias size="du -hs"
+
+################################
+###  File ShortCut
+###############################
+alias d="cd ~/Downloads"
+alias p="cd ~/Projects"
+alias h="cd /Users/usuario/"
+
+################################
+###  Program ShortCut
+################################
+
+# git related shortcut
+#More Used
+alias g="git"
+alias ga="git add ."
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gst="git status"
+alias gm="git merge"
+alias gp="git push"
+alias gb="git branch"
+alias gp="git pull"
+alias gco="git checkout"
+#Less Used
+alias gbd="git branch -D"
+alias gba="git branch -a"
+alias grs="git reset --soft"
+alias grh="git reset --hard"
+alias gcob="git checkout -b"
+alias gpom="git pull origin master"
